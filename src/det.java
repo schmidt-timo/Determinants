02f98bb2fc9fdc98b4e325200750610473d86977
Timo Schmidt <schmidt@timo.email>
2020-01-18 13:32:34 +0100

    Initial commit

---
+++ src/det.java
@@ -0,0 +1,19 @@
+
+public class det {
+
+	public det() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static double calcDetRecursiv(double[][] A) {
+		
+	}
+	public static double calcDet(double[][] A) {
+		
+	}
+}
