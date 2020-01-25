d1c8297eef0ab7a3a12e7b49d7df81d027f095cd
ThisIsKaluwa <aseriousmail@aol.com>
2020-01-25 13:53:54 +0100

    Normalform und Determinante daraus berechnen funktioniert

--- src/det.java
+++ src/det.java
@@ -4,69 +4,95 @@
  */
 
 public class det {
-    public static int nrOfMult;
-    
-    //Berechnung mit 1. Normalform
-    public static double calcDet(double[][] A){
-        return Double.NaN; // Durch Ihren Code ersetzen!
-    }
+	public static int nrOfMult;
 
-    //Rekursive Berechnung mit Def. L.4.1.1 Skript
-    // Source: http://professorjava.weebly.com/matrix-determinant.html
-    public static double calcDetRec(double[][] A) {
+	// Berechnung mit 1. Normalform
+	public static double calcDet(double[][] A) {
+		firstNormalForm(A);
+		
+		double det = 1;
+		for (int i = 0; i < A.length; i++) {
+			det *= A[i][i];
+		}
+		
+		return det;
+	}
 
-        // Länge der Matrix (Anzahl Spalten)
-        int len = A.length;
+	//Umformung von A in die erste Normalform
+	public static void firstNormalForm(double[][] A) {
+		
+		//Anzahl der Spalten
+		for (int j = 0; j < A.length; j++) {
+			//Anzahl der Zeilen
+			for (int i = j+1; i < A.length; i++) {
+				//Zahlen in der Diagonale
+				double a = A[j][j];
+				//Faktor zum berechnen der Nullen in der j-ten Spalte
+				double factor = A[i][j] / a;
+				
+				//Subtraktion der Zeilen
+				for (int k = 0; k < A.length; k++) {
+					A[i][k] = A[i][k] - factor*A[j][k];
+				}
+			}
+		}
+	}
 
-        // Determinate initialisieren
-        double det = 0;
+	// Rekursive Berechnung mit Def. L.4.1.1 Skript
+	// Source: http://professorjava.weebly.com/matrix-determinant.html
+	public static double calcDetRec(double[][] A) {
 
-        // Base case / Termination statement
-        if (len == 1)
-            return A[0][0];
+		// Länge der Matrix (Anzahl Spalten)
+		int len = A.length;
 
-        // Durch die Spalten der Matrix iterieren
-        for (int i = 0; i < len; i++) {
+		// Determinate initialisieren
+		double det = 0;
 
-            // Teilmatrix initialisieren
-            double[][] partial = new double[len-1][len-1];
+		// Base case / Termination statement
+		if (len == 1)
+			return A[0][0];
 
-            // Teilmatrix erstellen
-            for (int j = 1; j < len; j++) {
-                for (int k = 0; k < len; k++) {
-                    if (k < i)
-                        partial[j-1][k] = A[j][k];
-                    else if (k > i) {
-                        partial[j-1][k-1] = A[j][k];
-                    }
-                }
-            }
+		// Durch die Spalten der Matrix iterieren
+		for (int i = 0; i < len; i++) {
 
-            // Vorzeichen festlegen
-            int sign = i % 2 == 0 ? 1 : -1;
+			// Teilmatrix initialisieren
+			double[][] partial = new double[len - 1][len - 1];
 
-            // Rekursiver Aufruf der Methode mit der Teilmatrix
-            det+= sign * A[0][i] * calcDetRec(partial);
+			// Teilmatrix erstellen
+			for (int j = 1; j < len; j++) {
+				for (int k = 0; k < len; k++) {
+					if (k < i)
+						partial[j - 1][k] = A[j][k];
+					else if (k > i) {
+						partial[j - 1][k - 1] = A[j][k];
+					}
+				}
+			}
 
-        }
-        return det;
-    }
+			// Vorzeichen festlegen
+			int sign = i % 2 == 0 ? 1 : -1;
 
-    public static void main(String[] args) {
+			// Rekursiver Aufruf der Methode mit der Teilmatrix
+			det += sign * A[0][i] * calcDetRec(partial);
 
-        double[][] test = { // ergibt det = -6
-                { 1, 2, 0 },
-                { 2, 1, 3 },
-                { 1, 3, 1 },
-        };
+		}
+		return det;
+	}
 
-        System.out.println(calcDetRec(test));
+	public static void main(String[] args) {
 
-        // Test 1 rekursiv
-        testDet A = new testDet("src/Test.txt");
-        double[][] matrixA = A.getA();
-        System.out.println(calcDetRec(matrixA));
+		double[][] test = { // ergibt det = -6
+				{ 1, 2, 0 }, { 2, 1, 3 }, { 1, 3, 1 }, };
 
-    }
+		System.out.println(calcDetRec(test));
+		firstNormalForm(test);
+
+		// Test 1 rekursiv
+		testDet A = new testDet("src/Test.txt");
+		System.out.println(calcDet(test));
+		double[][] matrixA = A.getA();
+		System.out.println(calcDetRec(matrixA));
+
+	}
 
 }
